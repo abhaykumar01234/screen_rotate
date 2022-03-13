3404944bc13ecaa8a7579c738417329b24f06612
Abhay Kumar <abhaykumar01234@gmail.com>
2022-03-13 16:36:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <h1>Testing screen rotate</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+        cum! Explicabo est saepe hic laborum.
+      </p>
+      <div className="videoWrapper">
+        <div className="videoBox">
+          <iframe
+            src={`https://www.youtube.com/embed/ULdkpU51hTQ?autoplay=1&playsinline=1`}
+            title={"Youtube title"}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true}
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 }
 
